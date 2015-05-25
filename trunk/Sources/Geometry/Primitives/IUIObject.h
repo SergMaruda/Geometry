c395c6fc6d8384cfb4b2d6db80e5dde8655747ea
Sergey Maruda <maruda@ukr.net>
2015-05-25 11:35:01 +0300

    added typedef IUIObject* TIUIObjectPtr;

--- trunk/Sources/Geometry/Primitives/IUIObject.h
+++ trunk/Sources/Geometry/Primitives/IUIObject.h
@@ -1,20 +1,28 @@
 #pragma once
+#include <memory>
+
+
+interface IUIObject;
+
+typedef IUIObject* TIUIObjectPtr;
 
 interface IUIObject
   {
   virtual void SetLabel(const CString&) = 0;
-  virtual void SetParent(IUIObject*) = 0;
-  virtual void AddChild(IUIObject*) = 0;
-  virtual IUIObject* RemoveChild(size_t) = 0; //release ownership
+  virtual void SetParent(TIUIObjectPtr) = 0;
+  virtual void AddChild(TIUIObjectPtr) = 0;
+  virtual TIUIObjectPtr RemoveChild(size_t) = 0; //release ownership
   virtual void DeleteChild(size_t) = 0;//delete completely
-  virtual void DeleteChild(IUIObject*) = 0;//delete completely
+  virtual void DeleteChild(TIUIObjectPtr) = 0;//delete completely
   virtual void DeleteAllChilds() = 0;
 
   virtual const CString& GetLabel() const = 0;
-  virtual IUIObject*     GetParent() const = 0;
-  virtual IUIObject*     GetChild(size_t) const = 0;
+  virtual TIUIObjectPtr  GetParent() const = 0;
+  virtual TIUIObjectPtr  GetChild(size_t) const = 0;
   virtual size_t         GetNumChilds() const = 0;
   virtual COLORREF       GetColor() const = 0;
 
   virtual ~IUIObject() = 0 {};
   };
+
+
